16c6e8f5a17a6cbb5c9ad2045f7864ed2d15b030
gurmeet1109@gmail.com <gurmeet1109@gmail.com>
2015-11-12 15:22:35 +0000

    Updated GPIO -g tag

--- gpio.php
+++ gpio.php
@@ -2,7 +2,7 @@
 //Adapted from -> TheFreeElectron 2015, http://www.instructables.com/member/TheFreeElectron/
 
 //This page is requested by the JavaScript, it updates the pin's status and then print it
-
+//Pin numbers are BCM pin numbers (-g tag) and not WiringPi
 
 //Getting and using values
 if (isset ( $_GET["pin"] )) {
@@ -12,18 +12,18 @@ if (isset ( $_GET["pin"] )) {
 	if ( (is_numeric($pin)) && ($pin <= 40) && ($pin >= 0) ) {
 		
 		//set the gpio's mode to output		
-		system("gpio mode -g ".$pin." out");
+		system("gpio -g mode ".$pin." out");
 		
 		//reading pin's status
-		exec ("gpio read -g ".$pin, $status, $return );
+		exec ("gpio -g read ".$pin, $status, $return );
 		
 		//set the gpio to high/low
 		if ($status[0] == "0" ) { $status[0] = "1"; }
 		else if ($status[0] == "1" ) { $status[0] = "0"; }
-		system("gpio write -g ".$pin." ".$status[0] );
+		system("gpio -g write ".$pin." ".$status[0] );
 		
 		//reading pin's status
-		exec ("gpio read -g ".$pin, $status, $return );
+		exec ("gpio -g read ".$pin, $status, $return );
 		
 		//print it to the client on the response
 		echo($status[0]);
