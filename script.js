7c20562de0408a8f63ac8f478086c55be1ee920c
gurmeet1109@gmail.com <gurmeet1109@gmail.com>
2015-11-12 11:39:41 +0000

    Expanded Script for contain 12 elements

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 ﻿//TheFreeElectron 2015, http://www.instructables.com/member/TheFreeElectron/
 //JavaScript, uses pictures as buttons, sends and receives values to/from the Rpi
 //These are all the buttons
-var button_0 = document.getElementById("button_0");
+// var button_0 = document.getElementById("button_0");
 var button_1 = document.getElementById("button_1");
 var button_2 = document.getElementById("button_2");
 var button_3 = document.getElementById("button_3");
 var button_4 = document.getElementById("button_4");
 var button_5 = document.getElementById("button_5");
 var button_6 = document.getElementById("button_6");
-var button_7 = document.getElementById("button_7");
+var_button_7 = document.getElementById("button_7");
+var_button_8 = document.getElementById("button_8");
+var_button_9 = document.getElementById("button_9");
+var_button_10 = document.getElementById("button_10");
+var_button_11 = document.getElementById("button_11");
+var button_12 = document.getElementById("button_12");
+
+
 
 //Create an array for easy access later
-var Buttons = [ button_0, button_1, button_2, button_3, button_4, button_5, button_6, button_7];
+var Buttons = [ button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9, button_10, button_11, button_12 ];
 
 //This function is asking for gpio.php, receiving datas and updating the index.php pictures
 function change_pin ( pic ) {
