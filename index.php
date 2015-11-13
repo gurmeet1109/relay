b6c2657480f2081536316fadf5d9ef2824f8f80a
gurmeet1109@gmail.com <gurmeet1109@gmail.com>
2015-11-13 02:34:47 +0000

    Formatting change

--- index.php
+++ index.php
@@ -27,11 +27,11 @@
 	for ($i = 0; $i < 12; $i++) {
 		//if off
 		if ($val_array[$i][0] == 0 ) {
-			echo ("<img id='button_".$i."' src='data/img/red/red.jpg' onclick='change_pin (".$pin_array[$i].");'/>");
+			echo ("<img id='button_".$i."' src='data/img/red/red.jpg' onclick='change_pin(".$pin_array[$i].");'/>");
 		}
 		//if on
 		if ($val_array[$i][0] == 1 ) {
-			echo ("<img id='button_".$i."' src='data/img/green/green.jpg' onclick='change_pin (".$pin_array[$i].");'/>");
+			echo ("<img id='button_".$i."' src='data/img/green/green.jpg' onclick='change_pin(".$pin_array[$i].");'/>");
 		}	 
 	}
 	?>
