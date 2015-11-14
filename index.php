dff0854404577349023138c9a3aedec4a40c897d
gurmeet1109@gmail.com <gurmeet1109@gmail.com>
2015-11-14 15:50:17 +0000

    Removed debugging tags

--- index.php
+++ index.php
@@ -35,17 +35,17 @@
 	if ($val_array[$i][0] == 0 ) {
 	
 		echo ("<img id='button_".$i."' src='data/img/red/red.jpg' onclick='change_pin($pin_array[$i]);'/>");
-		echo ( $pin_array[$i] );
-		echo ( "&nbsp" );
-		echo ( $val_array[$i][0] );
+//		echo ( $pin_array[$i] );
+//		echo ( "&nbsp" );
+//		echo ( $val_array[$i][0] );
 	}
 
 	//if on
 	if ($val_array[$i][0] == 1 ) {
 	echo ("<img id='button_".$i."' src='data/img/green/green.jpg' onclick='change_pin($pin_array[$i]);'/>");
-		echo ( $pin_array[$i] );
-		echo ( "&nbsp" );
-		echo ( $val_array[$i][0] );
+//		echo ( $pin_array[$i] );
+//		echo ( "&nbsp" );
+//		echo ( $val_array[$i][0] );
 	}
 
 	if( $i%4 == 0) {
