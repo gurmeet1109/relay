6f0e26c0fa3cc30df24f0fe84748013ac53ecdfd
gurmeet1109@gmail.com <gurmeet1109@gmail.com>
2015-11-14 19:09:47 +0000

    HTML Changes - Incorported Devices

--- index.php
+++ index.php
@@ -7,14 +7,17 @@
         <title>Relay Module Control Panel</title>
     </head>
  
-    <body style="background-color:black;">
+    <body style="background-color:gray;">
 
     <!-- On/Off button's picture -->
 	<?php
 	
 	$pin_array = array(17,27,22,5,18,23,24,25,12,13,19,26);
 	$val_array = array(0,0,0,0,0,0,0,0,0,0,0,0);
-	
+	$dev_array = array("Motor", "Pedestal Fan", "CFL", "Cooler", "Chanting Machine", "Bulb Cluster", "Incasedent Bulb", "LED Lamp", "Bulb Cluster", "Table 
+Fan", "Ceiling Fan", "Tube Light" );
+	$surge_array = array("Surge Protection - IC", "Surge Protection - IC", "Surge Protection - IC", "Surge Protection - IC", "Unprotected", "Unprotected", 
+"Unprotected", "Unprotected", "Surge Protection - RC", "Surge Protection - RC", "Surge Protection - RC", "Surge Protection - RC");  
 
 	//this php script generate the first page in function of the file
 	for ( $i= 0; $i<12; $i++) {
@@ -35,7 +38,8 @@
 	//if off
 	if ($val_array[$i][0] == 0 ) {
 	
-		echo ("<td><img id='button_".$i."' src='data/img/red/red.jpg' onclick='change_pin($pin_array[$i]);'/></td>");
+		echo ("<td><img id='button_".$i."' src='data/img/red/red.jpg' onclick='change_pin($pin_array[$i]);'/><br>$dev_array[$i]<br>$surge_array[$i] 
+<br>.</td>");
 //		echo ( $pin_array[$i] );
 //		echo ( "&nbsp" );
 //		echo ( $val_array[$i][0] );
@@ -43,7 +47,9 @@
 
 	//if on
 	if ($val_array[$i][0] == 1 ) {
-	echo ("<td><img id='button_".$i."' src='data/img/green/green.jpg' onclick='change_pin($pin_array[$i]);'/></td>");
+	echo ("<td><img id='button_".$i."' src='data/img/green/green.jpg' 
+onclick='change_pin($pin_array[$i]);'/><br>$dev_array[$i]<br>$surge_array[$i]<br>.</td>");
+
 //		echo ( $pin_array[$i] );
 //		echo ( "&nbsp" );
 //		echo ( $val_array[$i][0] );
